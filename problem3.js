0c5ae8d40daf25924692851ff4de7fc78b0748aa
Taxato <taxatoftw@gmail.com>
2023-12-04 10:32:22 +0100

    3

---
+++ problem3.js
@@ -0,0 +1,12 @@
+// Largest Prime Factor
+// What is the largest prime factor of the number 600851475143?
+
+import { isPrime } from "./helpers";
+
+const num = 600851475143;
+let max;
+for (let i = 1; i <= Math.sqrt(num); i++) {
+	if (num % i === 0 && isPrime(i)) max = i;
+}
+
+console.log(max);
