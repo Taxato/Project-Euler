506e0836adc731b66cbb3795bade7b9a517cc018
Taxato <taxatoftw@gmail.com>
2023-12-04 10:23:36 +0100

    2

---
+++ problem2.js
@@ -0,0 +1,20 @@
+// Even Fibonacci Numbers
+// By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
+function fibonacci(n, cache = {}) {
+	if (n < 2) return 1;
+	else if (n in cache) return cache[n];
+
+	cache[n] = fibonacci(n - 1, cache) + fibonacci(n - 2, cache);
+
+	return cache[n];
+}
+
+const cache = {};
+let sum = 0;
+let num = 0;
+for (let i = 1; num < 4e6; i++) {
+	num = fibonacci(i, cache);
+	if (/[02468]$/.test(num + "")) sum += num;
+}
+console.log(sum);
